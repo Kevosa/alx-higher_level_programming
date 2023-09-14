c4453cb930dce101c99179c7a03a20e1c4cd0c83
Kevosa <yegokelvin00@gmail.com>
2023-09-14 09:36:20 +0000

    fixing minor errors

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print("{}".format(chr(i + 97) if i % 2 == 1 else chr(i + 65)), end="")
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
-for i in range(0, 8):
+#!/usr/bin/python3
+for i in range(9):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+        print("{:d}{:d}".format(i, j),
+              end="\n" if (i == 8 and j == 9) else ", ")
